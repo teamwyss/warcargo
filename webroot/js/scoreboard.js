76c0045baeb39ae0fd07ca6451d9e7e2c47765cb
DavidWyss <dmwyss@gmail.com>
2019-04-27 11:59:34 +1000

    Made scoreboard a bit fancier.

--- webroot/js/scoreboard.js
+++ webroot/js/scoreboard.js
@@ -4,13 +4,21 @@ var scoreboard = {
 	score: 0,
 	bacon: 0,
 	scoreText: null,
+	bg: null,
 	oTextStyle: { fontSize: '32px', fill: '#FFF', fontWeight: 20 },
 	init: function(game) {
 		var iPosX = 16;
 		var iPosY = 16;
 		var iLineH = 40;
 		var sColor = "#FFF"
-		
+
+			
+		// DO not delete, may be useful var msgBox = game.add.group();
+        //make the back of the message box
+        this.bg = game.add.sprite(0, 0, "bgScoreboard");
+        this.bg.alpha = 0.5;
+        this.bg.fixedToCamera = true;
+			
 		// Score
 		this.scoreText = game.add.text(iPosX, iPosY, 'score: 0', this.oTextStyle);
 		this.scoreText.fixedToCamera = true;
