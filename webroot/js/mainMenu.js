aa2d3230dede2f0786c880ac1e498d20038caffa
DavidWyss <dmwyss@gmail.com>
2019-05-17 23:55:04 +1000

    Neatened up scoreboard, and put in opening and closing logic.

--- webroot/js/mainMenu.js
+++ webroot/js/mainMenu.js
@@ -1,15 +1,24 @@
 
 var mainMenu ={
 	ui: null
+	,uiBgScreen: null
 	,init: function() {
 		this.ui = document.querySelector("#mainMenu-frame");
+		this.uiBgScreen = document.querySelector("#mainMenu-bgScreen");
+		return this;
+	}
+	,showOrHide: function(isShow) {
+		var sDisplay = isShow ? "block" :  "none"
+		this.ui.style.display = sDisplay;
+		this.uiBgScreen.style.display = sDisplay;
 		return this;
 	}
 	,show: function() {
+		this.showOrHide(true);
 		return this;
 	}
 	,hide: function() {
-		this.ui.style.display = "none";
+		this.showOrHide(false);
 		return this;
 	}
 }
